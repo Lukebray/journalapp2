b9a296e254477832d832ff29e5157ee8b3f480b9
Lukebray <39488446+Lukebray@users.noreply.github.com>
2020-06-21 23:19:47 +0100

    basic views

--- mysite/journalapp/urls.py
+++ mysite/journalapp/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from . import views
 
+
+app_name = journalapp
 urlpatterns = [
-    path('', views.index, name='index')
+    #ex: /
+    path('', views.index, name='index'),
+
+    #ex: /home/1/
+    path('home/<int:user_id>/', views.home, name='home'),
+
+    #ex: /home/1/2/
+    path('home/<int:user_id>/<int:journal_id>/', views.journal_view, name='journal_view'),
+
+    #ex: /home/1/2/6/
+    path('home/<int:user_id>/<int:journal_id>/<int:entry_id>/', views.entry_view, name='entry_view'),
+
+    #ex: /home/1/2/
+    path('home/<int:user_id>/<int:journal_id>/new/', views.new_entry, name='new_entry'),
 ]
--- mysite/journalapp/views.py
+++ mysite/journalapp/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import User, Journal, Entry
 
 def index(request):
-    return HttpResponse("Hello there!")
+    return HttpResponse("This will be the login page")
 
-# Create your views here.
+def home(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    template = loader.get_template('journals/home.html')
+    context = {
+        'user': user
+    }
+    return render(request, 'journals/home.html', context)
+
+def journal_view(request, user_id, journal_id):
+    journal = Journal.objects.get(pk=journal_id)
+    return HttpResponse("Logged in as %s. Viewing journal %s." % (user_id, journal.journal_name))
+
+def entry_view(request, journal_id, user_id, entry_id):
+    return HttpResponse("You can read the entry here. You are reading entry id %s from journal %s logged in as user %s." % (entry_id, journal_id, user_id))
+
+def new_entry(request, user_id, journal_id):
+    return HttpResponse("You input your entry here. You are inputting into journal id %s logged in as user id %s." % (journal_id, user_id))
